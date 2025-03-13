bc83fc1d88e4a9c44ea625a7ae99814608111580
Arthurnacious <arthurnacious@gmail.com>
2025-03-13 22:14:14 +0200

    fix: Navlink not showing bg color on nested routes

--- src/components/app/sidebar/nav-item.tsx
+++ src/components/app/sidebar/nav-item.tsx
@@ -21,7 +21,8 @@ const NavItem: FC<Props> = ({ href, icon: Icon, children, onClick }) => {
       className={cn(
         "flex items-center px-3 py-2 text-sm rounded-md transition-colors text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-gray-50 dark:hover:bg-[#1F1F23]",
         {
-          "bg-gray-50 dark:bg-[#1F1F23]": pathname === href,
+          "bg-gray-50 dark:bg-[#1F1F23]":
+            (href !== "/" && pathname.startsWith(href)) || pathname === href,
         }
       )}
     >
